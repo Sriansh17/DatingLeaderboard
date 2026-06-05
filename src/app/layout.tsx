63a7dd2a035d1b8ab757b3b339173c21c159af00
Sriansh17 <rajritulrajrazi@gmail.com>
2026-06-05 21:40:20 +0530

    fix: move footer inside content column to remove right whitespace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,12 +45,14 @@ export default function RootLayout({
               <ToastProvider>
                 <div className="min-h-screen flex pb-16 md:pb-0">
                   <Sidebar />
-                  <main className="flex-1 min-w-0 overflow-x-hidden">
-                    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                      {children}
-                    </div>
-                  </main>
-                  <Footer />
+                  <div className="flex-1 flex flex-col min-w-0">
+                    <main className="flex-1 overflow-x-hidden">
+                      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                        {children}
+                      </div>
+                    </main>
+                    <Footer />
+                  </div>
                   <BottomNav />
                 </div>
               </ToastProvider>
